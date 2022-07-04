3bb8cd7f22976132bbb00496513101985293b12e
iTheFoxy <dv128@DESKTOP-4GMKOAI>
2022-07-04 23:56:23 +1000

    Добавьте файлы проекта.

---
+++ TestSerialize/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Serialize
+{
+    class Program
+    {
+
+      
+        static void Main(string[] args)
+        {
+            Person tom = new Person() { Name = "Tom", Age = 35, Money = 3242.324M };
+            var str = Serializer.Serialize(tom);
+            Console.WriteLine(str);
+            var restoredPerson = Serializer.Deserialize<Person>(str);
+            Console.WriteLine($"Name: {restoredPerson.Name}  Age: {restoredPerson.Age}");
+            Console.ReadKey();
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public decimal Money { get; set; }
+    }
+}
---
+++ TestSerialize/Serializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace Serialize
+{
+    public class Serializer
+    {
+        public static T Deserialize<T>(string s)
+        {
+            var obj = Activator.CreateInstance(typeof(T));
+            foreach (var item in s.Split('\n'))
+            {
+                if (!string.IsNullOrEmpty(item))
+                {
+                    var type = obj.GetType().GetProperty(item.Split('=')[0]).PropertyType;
+                    var value = Convert.ChangeType(item.Split('=')[1], type);
+                    obj.GetType().GetProperty(item.Split('=')[0]).SetValue(obj, value);
+                }
+            }
+            return (T)obj;
+        }
+        public static string Serialize<T>(T obj)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            var a = obj.GetType().GetProperties();
+            foreach (var item in a)
+            {
+                stringBuilder.Append(item.Name + "=" + item.GetValue(obj) + '\n');
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
